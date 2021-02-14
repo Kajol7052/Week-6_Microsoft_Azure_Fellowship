a39387176a44ad0e348995d74ed32fc63df20b38
Kajol7052 <kajol7052@gmail.com>
2021-02-14 21:01:32 +0530

    Added: DataFactory.py class

---
+++ Microsoft_Azure_Fellowship/DataFactory.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+# @Author: Kajol.Patira
+# @Date:   2021-02-03 21:29:20
+# @Last Modified by:   Kajol.Patira
+# @Last Modified time: 2021-02-04 01:37:51
+
+from azure.identity import ClientSecretCredential 
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.datafactory import DataFactoryManagementClient
+from azure.mgmt.datafactory.models import *
+from datetime import datetime, timedelta
+import time
+
+def print_item(group):
+    """Print an Azure object instance."""
+    print("\tName: {}".format(group.name))
+    print("\tId: {}".format(group.id))
+    if hasattr(group, 'location'):
+        print("\tLocation: {}".format(group.location))
+    if hasattr(group, 'tags'):
+        print("\tTags: {}".format(group.tags))
+    if hasattr(group, 'properties'):
+        print_properties(group.properties)
+
+def print_properties(props):
+    """Print a ResourceGroup properties instance."""
+    if props and hasattr(props, 'provisioning_state') and props.provisioning_state:
+        print("\tProperties:")
+        print("\t\tProvisioning State: {}".format(props.provisioning_state))
+    print("\n\n")
+
+def print_activity_run_details(activity_run):
+    """Print activity run details."""
+    print("\n\tActivity run details\n")
+    print("\tActivity run status: {}".format(activity_run.status))
+    if activity_run.status == 'Succeeded':
+        print("\tNumber of bytes read: {}".format(activity_run.output['dataRead']))
+        print("\tNumber of bytes written: {}".format(activity_run.output['dataWritten']))
+        print("\tCopy duration: {}".format(activity_run.output['copyDuration']))
+    else:
+        print("\tErrors: {}".format(activity_run.error['message']))
+
+
+def main():
+
+    # Azure subscription ID
+    subscription_id = '94b45640-9caa-4aee-9764-8b9d08ba2c67'
+
+    # This program creates this resource group. If it's an existing resource group, comment out the code that creates the resource group
+    rg_name = 'kajolresourcegroup'
+
+    # The data factory name. It must be globally unique.
+    df_name = 'kajoldf5'
+
+    # Specify your Active Directory client ID, client secret, and tenant ID
+    credentials = ClientSecretCredential(client_id='2ecaab02-43a5-41cb-a853-57373fa8e279', client_secret='hiUTL3nVG5.-1D6.D_nqm6g24.w4JWpToA', tenant_id='0e8f25a5-1b3a-4dcc-83a7-46555e7ef4e2') 
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    adf_client = DataFactoryManagementClient(credentials, subscription_id)
+
+    rg_params = {'location':'southindia'}
+    df_params = {'location':'southindia'}
+ 
+    # create the resource group
+    # comment out if the resource group already exits
+    #resource_client.resource_groups.create_or_update(rg_name, rg_params)
+
+    # Create a data factory
+    df_resource = Factory(location='southindia')
+    df = adf_client.factories.create_or_update(rg_name, df_name, df_resource)
+    print_item(df)
+    while df.provisioning_state != 'Succeeded':
+        df = adf_client.factories.get(rg_name, df_name)
+        time.sleep(1)
+
+    # Create an Azure Storage linked service
+    ls_name = 'storageLinkedService001'
+
+    # IMPORTANT: specify the name and key of your Azure Storage account.
+    storage_string = SecureString(value='DefaultEndpointsProtocol=https;AccountName=kajolstorageaccount;AccountKey=I5Yoff/0pAaNbvnMG+UByaEOWe1t6Xnil6cqwGt+mo9a63BDHXJ4mtK8wyC+q7dH5qfibsc+O94Sp3XM94KHDg==;EndpointSuffix=core.windows.net')
+
+    ls_azure_storage = LinkedServiceResource(properties=AzureStorageLinkedService(connection_string=storage_string)) 
+    ls = adf_client.linked_services.create_or_update(rg_name, df_name, ls_name, ls_azure_storage)
+    print_item(ls)
+
+    # Create an Azure blob dataset (input)
+    ds_name = 'ds_in'
+    ds_ls = LinkedServiceReference(reference_name=ls_name)
+    blob_path = 'kajolcontainer'
+    blob_filename = 'input.txt'
+    ds_azure_blob = DatasetResource(properties=AzureBlobDataset(
+        linked_service_name=ds_ls, folder_path=blob_path, file_name=blob_filename))
+    ds = adf_client.datasets.create_or_update(
+        rg_name, df_name, ds_name, ds_azure_blob)
+    print_item(ds)
+
+    # Create an Azure blob dataset (output)
+    dsOut_name = 'ds_out'
+    output_blobpath = 'kajolcontainer/output'
+    dsOut_azure_blob = DatasetResource(properties=AzureBlobDataset(linked_service_name=ds_ls, folder_path=output_blobpath))
+    dsOut = adf_client.datasets.create_or_update(
+        rg_name, df_name, dsOut_name, dsOut_azure_blob)
+    print_item(dsOut)
+
+    # Create a copy activity
+    act_name = 'copyBlobtoBlob'
+    blob_source = BlobSource()
+    blob_sink = BlobSink()
+    dsin_ref = DatasetReference(reference_name=ds_name)
+    dsOut_ref = DatasetReference(reference_name=dsOut_name)
+    copy_activity = CopyActivity(name=act_name, inputs=[dsin_ref], outputs=[
+                                 dsOut_ref], source=blob_source, sink=blob_sink)
+
+    # Create a pipeline with the copy activity
+    p_name = 'copyPipeline'
+    params_for_pipeline = {}
+    p_obj = PipelineResource(
+        activities=[copy_activity], parameters=params_for_pipeline)
+    p = adf_client.pipelines.create_or_update(rg_name, df_name, p_name, p_obj)
+    print_item(p)
+
+    # Create a pipeline run
+    run_response = adf_client.pipelines.create_run(rg_name, df_name, p_name, parameters={})
+
+    # Monitor the pipeline run
+    time.sleep(30)
+    pipeline_run = adf_client.pipeline_runs.get(
+        rg_name, df_name, run_response.run_id)
+    print("\n\tPipeline run status: {}".format(pipeline_run.status))
+    filter_params = RunFilterParameters(
+        last_updated_after=datetime.now() - timedelta(1), last_updated_before=datetime.now() + timedelta(1))
+    query_response = adf_client.activity_runs.query_by_pipeline_run(
+        rg_name, df_name, pipeline_run.run_id, filter_params)
+    print_activity_run_details(query_response.value[0])
+
+
+# Start the main method
+main()
+
